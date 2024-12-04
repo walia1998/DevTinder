9b10f8a9bd3b1b1112ca17314ced3901e3c00d45
Ashish Walia <waliaashu1998@gmail.com>
2024-12-04 19:48:21 +0530

    add APi Calls

--- app.js
+++ app.js
@@ -6,19 +6,63 @@ const app = express();
 
 const User = require("./src/models/user.js");
 
-
 app.use(express.json());
 
-app.post("/signup" , async (req,res) => {
+app.post("/signup", async (req, res) => {
   const user = new User(req.body);
-
   try {
     await user.save();
     res.send("User Added Successfully");
   } catch (error) {
-    res.status(400).send("Error saving the user:" + err.message)
+    res.status(400).send("Error saving the user:" + err.message);
   }
-})
+});
+
+app.get("/user", async (req, res) => {
+  const userEmail = req.body._id;
+  try {
+    console.log(userEmail);
+    const users = await User.findById({ _id: userEmail });
+    res.send(users);
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+//Api - Feed API - GET /feed -get all the users from the database
+app.get("/feed", async (req, res) => {
+  try {
+    await User.find({});
+    res.send(user);
+  } catch (error) {
+    res.status(400).send("Error saving the user:" + err.message);
+  }
+});
+
+//Delete a user from the database
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    //const user = await User.findByIdAndDelete(_id : userId); We can use this also.
+    await User.findByIdAndDelete(userId);
+    res.send("User Deleted successfully");
+  } catch (error) {
+    res.status(400).send("Error saving the user:" + error.message);
+  }
+});
+
+//Update a user detail of the user
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+
+  try {
+    await User.findByIdAndUpdate({ _id: userId }, data);
+    res.send("User Update Successfully");
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+});
 
 connectDB()
   .then(() => {
@@ -31,5 +75,3 @@ connectDB()
   .catch((err) => {
     console.error("Database cannot be connected", err.message);
   });
-
-
